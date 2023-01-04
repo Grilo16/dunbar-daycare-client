6b5d258dbd85bc7300e2cea7d0490501d16fd45c
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2023-01-04 17:35:31 +0000

    nice base set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter as Router, Routes, Route, Link} from "react-router-dom"
+import FooterContainer from "./containers/FooterContainer";
+import HeaderContainer from "./containers/HeaderContainer";
+import Homepage from "./pages/Homepage";
+import TestPage from "./pages/TestPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+    <nav>
+      <Link to={"/"}>Homepage </Link>
+      <Link to={"/test"}>Test page </Link>
+    </nav>
+    <HeaderContainer/>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/test" element={<TestPage/>} />
+      </Routes>
+      <FooterContainer/>
+    </Router>
+
+  )
 }
 
 export default App;
